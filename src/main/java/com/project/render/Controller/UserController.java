84baa97d943792b0ca550ed7425afcb0b6cbf7d7
Yashraj <parmaryashraj068@gmail.com>
2025-11-16 22:29:28 +0530

    added

--- src/main/java/com/project/render/Controller/UserController.java
+++ src/main/java/com/project/render/Controller/UserController.java
@@ -6,13 +6,11 @@ import com.project.render.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 
