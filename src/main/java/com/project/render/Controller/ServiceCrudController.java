f9353331561986a558ca448f8f4ac465df874dc7
Yashraj <tmkoc1535@gmail.com>
2025-12-25 22:02:32 +0530

    service added

--- src/main/java/com/project/render/Controller/ServiceCrudController.java
+++ src/main/java/com/project/render/Controller/ServiceCrudController.java
@@ -5,6 +5,8 @@ import com.project.render.Service.ServiceCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/service")
 public class ServiceCrudController {
@@ -12,7 +14,13 @@ public class ServiceCrudController {
     @Autowired
     private ServiceCrudService serviceCrudService;
 
-
+    @GetMapping("/get-services")
+    public List<Service> getService(@RequestParam String salonId,
+                                    @RequestParam String categoryId,
+                                    @RequestParam(required = false,defaultValue = "all") String gender
+    ){
+        return serviceCrudService.getService(salonId, categoryId, gender);
+    }
 
     @PostMapping("/add-service/{serviceCategoryId}")
     public Service addService(@PathVariable String serviceCategoryId, @RequestBody Service service){
