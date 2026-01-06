1c8dc477d86e651efd712fa3f503ea1db9be2b02
Yashraj <tmkoc1535@gmail.com>
2026-01-06 14:53:48 +0530

    category drop down API added

--- src/main/java/com/project/render/Controller/ServiceCategoryController.java
+++ src/main/java/com/project/render/Controller/ServiceCategoryController.java
@@ -1,10 +1,12 @@
 package com.project.render.Controller;
 
+import com.project.render.DTO.ServiceCategoryDropdownDTO;
 import com.project.render.Entity.ServiceCategory;
 import com.project.render.Service.ServiceCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,6 +19,11 @@ public class ServiceCategoryController {
     @PostMapping("/add-service-category/{salonId}")
     public ServiceCategory addService(@PathVariable String salonId, @RequestBody ServiceCategory serviceCategory) {
         return serviceCategoryService.addService(salonId, serviceCategory);
+    }
+
+    @GetMapping("/get-service-categories/{salonId}")
+    public List<ServiceCategoryDropdownDTO> getServices(@PathVariable String salonId){
+        return serviceCategoryService.getCategoryBySalonId(salonId);
     }
 
     @GetMapping("/get-service-category/{serviceId}")
--- src/main/java/com/project/render/Service/ServiceCategoryService.java
+++ src/main/java/com/project/render/Service/ServiceCategoryService.java
@@ -1,5 +1,6 @@
 package com.project.render.Service;
 
+import com.project.render.DTO.ServiceCategoryDropdownDTO;
 import com.project.render.Entity.Salon;
 import com.project.render.Entity.ServiceCategory;
 import com.project.render.Repository.SalonRepository;
@@ -7,6 +8,7 @@ import com.project.render.Repository.ServiceCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @org.springframework.stereotype.Service
@@ -38,5 +40,26 @@ public class ServiceCategoryService {
 
     public Optional<ServiceCategory> getService(String serviceId) {
         return serviceCategoryRepository.findById(serviceId);
+    }
+
+    public List<ServiceCategoryDropdownDTO> getCategoryBySalonId(String salonId){
+        Salon salon = salonRepository.findById(salonId).orElseThrow(()-> new RuntimeException("Salon not found!"));
+        List<ServiceCategoryDropdownDTO> response = new ArrayList<>();
+
+        if(salon.getServiceIds()==null) return response;
+
+        for(String categoryId:salon.getServiceIds()){
+
+            serviceCategoryRepository.findById(categoryId).ifPresent(category ->
+                response.add(new ServiceCategoryDropdownDTO(
+                        category.getId(),
+                        category.getName()
+                    )
+                )
+            );
+
+        }
+
+        return response;
     }
 }
